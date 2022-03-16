4c08d4bd62636aaf7060ad96e994413e144dae7c
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2022-03-15 21:02:57 -0400

    Updated with per-team averages and summary stats

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Process-Scouting-Data
+
+Installation Instructions:
+1.) Clone this repository with HTTPS (not SSH or Git CLI)
--- process_data.py
+++ process_data.py
@@ -10,8 +10,8 @@ from dataclasses import dataclass, field
 ####################
 
 
-input_file_name = "sample_test_data.csv"
-output_file_name = "output_team_data.xlsx"
+input_file_name = "input.csv"
+output_file_name = "output_data.xlsx"
 
 all_team_match_entries = []
 all_team_data = []
@@ -19,8 +19,9 @@ team_num_list = []
 output_worksheets = []
 
 MAX_NUMBER_OF_QUAL_MATCHES = 15
-STATISTICS_START_ROW = 16
-STATISTICS_START_COL = 2
+AVERAGES_ROW = 16
+STATISTICS_START_ROW = 18
+STATISTICS_START_COL = 0
 CHART_START_ROW = 25
 CHART_ROW_SPACING = 10
 FIRST_CHART_COL = "A"
@@ -30,8 +31,8 @@ CHART_RED = "#EA5545"
 CHART_BLUE = "#27AEEF"
 
 # For a single robot
-MAX_POSSIBLE_AUTO_POINTS = 20
-MAX_POSSIBLE_TELE_POINTS = 60
+MAX_POSSIBLE_AUTO_POINTS = 10
+MAX_POSSIBLE_TELE_POINTS = 30
 
 
 #####################
@@ -40,22 +41,25 @@ MAX_POSSIBLE_TELE_POINTS = 60
 
 
 taxi_completed_dict = {
-    "Yes": True,
-    "No": False,
+    "Yes": 1,
+    "No": 0,
+    "": -1,
 }
 
 hangar_level_dict = {
     "No Hang": 0,
-    "Low Rung (1)": 1,
-    "Mid Rung (2)": 2,
-    "High Rung (3)": 3,
-    "Traversal Rung (4)": 4,
+    "Low Rung (1)": 4,
+    "Mid Rung (2)": 6,
+    "High Rung (3)": 10,
+    "Traversal Rung (4)": 15,
+    "": -1
 }
 
 defense_level_dict = {
     "No": 0,
     "Unsure": 1,
     "Yes": 2,
+    "": -1,
 }
 
 
@@ -78,6 +82,11 @@ class SingleTeamSingleMatchEntry:
 class TeamData:
     team_num: int = 0
     match_data: list = field(default_factory=list)
+    taxi_percent: float = 0
+    avg_auto_points: float = 0
+    avg_tele_points: float = 0
+    avg_defense_equivalent: float = 0
+    avg_climb_points: float = 0
 
 
 #############
@@ -95,6 +104,8 @@ def get_max_value_from_comma_separated_numbers(numbers):
         # Sometimes the split() function above leaves blank entries, this handles that issue
         if num != "":
             number_int_list.append(int(num))
+        else:
+            number_int_list.append(0)
 
     # Just in case number_int_list is empty, add -1 so that it will be the max number and
     # indicate that there was an error
@@ -104,6 +115,34 @@ def get_max_value_from_comma_separated_numbers(numbers):
     max_number = max(number_int_list)
 
     return max_number
+
+
+def parse_team_number(num):
+    # The team number could come in as a poorly formatted string or a number, this function
+    # helps standardize the input
+    if type(num) == int:
+        parsed_num = num
+    elif type(num) == str:
+        if num != "":
+            parsed_num = int(float(num))
+        else:
+            parsed_num = -1
+
+    return parsed_num
+
+
+def parse_match_number(num):
+    # The match number could come in as a poorly formatted string or a number, this function
+    # helps standardize the input
+    if type(num) == int:
+        parsed_num = num
+    elif type(num) == str:
+        if num != "":
+            parsed_num = int(float(num))
+        else:
+            parsed_num = -1
+
+    return parsed_num
 
 
 ######################
@@ -127,9 +166,10 @@ with open(input_file_name, "r", newline="") as input_csv_file:
         if row_num > 0:
             # Put all of the information from a single row in excel into a python object
             # called "team_match_entry" to make it easier to deal with later on
+            print(f"Processing Row Number {row_num + 1}")
             team_match_entry = SingleTeamSingleMatchEntry(
-                team_num=int(row_data[3]),
-                qual_match_num=int(row_data[4]),
+                team_num=parse_team_number(row_data[3]),
+                qual_match_num=parse_match_number(row_data[4]),
                 successfully_completed_taxi=taxi_completed_dict[row_data[5]],
                 auto_cargo_scored_upper=get_max_value_from_comma_separated_numbers(
                     row_data[6]),
@@ -147,12 +187,18 @@ with open(input_file_name, "r", newline="") as input_csv_file:
             # Add the single-team single-match entry (i.e. data from one row) to a list
             # containing all of these entries
             all_team_match_entries.append(team_match_entry)
+        else:
+            print("Skipping Row Number 1 (Column Titles)")
+
 
 # Go through every match entry one-by-one and check if a class for all of a team's matches
 # has been created yet (the TeamData class). If not, create it and add it to a list of
 # these classes. Then, add the current match entry to its corresponding class containing all
 # of that team's match entries.
 for match_entry in all_team_match_entries:
+    # If the team num is -1 (due to an error), skip this iteration of the for loop
+    if match_entry.team_num == -1:
+        continue
     if match_entry.team_num not in team_num_list:
         team_num_list.append(match_entry.team_num)
 
@@ -229,11 +275,21 @@ with xlsxwriter.Workbook(output_file_name) as output_workbook:
                 single_teams_worksheet.set_column_pixels(7, 7, 90)    # Defense
                 single_teams_worksheet.set_column_pixels(8, 8, 1000)  # Other
 
+                # These variables keep track of the total points scored in each category
+                # so that averages can be calculated later
+                team_total_taxi_equivalent = 0
+                team_total_auto_cargo_upper = 0
+                team_total_auto_cargo_lower = 0
+                team_total_tele_cargo_upper = 0
+                team_total_tele_cargo_lower = 0
+                team_total_defense_equivalent = 0
+                team_total_climb_points = 0
+
                 # Add the team's match data to that team's worksheet
                 for i, match in enumerate(single_teams_data.match_data):
                     # Turn the python representation of the match data back into
                     # human-friendly text that can be written to the spreadsheet
-                    taxi_string = "Yes" if match.successfully_completed_taxi else "No"
+                    taxi_string = "Yes" if match.successfully_completed_taxi == 1 else "No"
 
                     hangar_string = "ERROR"
                     for key, value in hangar_level_dict.items():
@@ -266,12 +322,79 @@ with xlsxwriter.Workbook(output_file_name) as output_workbook:
                     single_teams_worksheet.write(
                         i + 1, 8, match.other_info)
 
+                    # Add up the team total numbers across all matches
+                    team_total_taxi_equivalent += match.successfully_completed_taxi
+                    team_total_auto_cargo_upper += match.auto_cargo_scored_upper
+                    team_total_auto_cargo_lower += match.auto_cargo_scored_lower
+                    team_total_tele_cargo_upper += match.tele_cargo_scored_upper
+                    team_total_tele_cargo_lower += match.tele_cargo_scored_lower
+                    team_total_defense_equivalent += match.defense_level
+                    team_total_climb_points += match.hangar_level
+
                 # Get the current number of matches this team has completed
                 current_match_count = len(single_teams_data.match_data)
 
+                # Create averages from totals
+                team_avg_taxi_percent = team_total_taxi_equivalent / current_match_count
+                team_avg_auto_cargo_upper = team_total_auto_cargo_upper / current_match_count
+                team_avg_auto_cargo_lower = team_total_auto_cargo_lower / current_match_count
+                team_avg_tele_cargo_upper = team_total_tele_cargo_upper / current_match_count
+                team_avg_tele_cargo_lower = team_total_tele_cargo_lower / current_match_count
+                team_avg_defense_equivalent = team_total_defense_equivalent / current_match_count
+                team_avg_climb_points = team_total_climb_points / current_match_count
+
+                # Update general team statistics based on these averages
+                single_teams_data.taxi_percent = team_avg_taxi_percent
+                single_teams_data.avg_auto_points = 2 * team_avg_taxi_percent + \
+                    2 * team_avg_auto_cargo_lower + 4 * team_avg_auto_cargo_upper
+                single_teams_data.avg_tele_points = team_avg_tele_cargo_lower + \
+                    2 * team_avg_auto_cargo_upper
+                single_teams_data.avg_defense_equivalent = team_avg_defense_equivalent
+                single_teams_data.avg_climb_points = team_avg_climb_points
+
+                # Print averages to the spreadsheet
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 0, "Averages:")
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 1, team_avg_taxi_percent)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 2, team_avg_auto_cargo_upper)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 3, team_avg_auto_cargo_lower)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 4, team_avg_tele_cargo_upper)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 5, team_avg_tele_cargo_lower)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 6, team_avg_climb_points)
+                single_teams_worksheet.write(
+                    AVERAGES_ROW, 7, team_avg_defense_equivalent)
+
                 # Summary statistics
                 single_teams_worksheet.write(
-                    STATISTICS_START_ROW, STATISTICS_START_COL, "")
+                    STATISTICS_START_ROW, STATISTICS_START_COL, "Taxi Percentage: ")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW, STATISTICS_START_COL + 1, single_teams_data.taxi_percent)
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 1, STATISTICS_START_COL, "Avg. Auto Points: ")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 1, STATISTICS_START_COL + 1, single_teams_data.avg_auto_points)
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 1, STATISTICS_START_COL + 2, "(Including avg. taxi points)")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 2, STATISTICS_START_COL, "Avg. Teleop Points: ")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 2, STATISTICS_START_COL + 1, single_teams_data.avg_tele_points)
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 3, STATISTICS_START_COL, "Avg. Defense: ")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 3, STATISTICS_START_COL + 1, single_teams_data.avg_defense_equivalent)
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 3, STATISTICS_START_COL + 2, "(2 = Yes/Always, 1 = Unsure/Mixed, 0 = No/Never)")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 4, STATISTICS_START_COL, "Avg. Climb Points: ")
+                single_teams_worksheet.write(
+                    STATISTICS_START_ROW + 4, STATISTICS_START_COL + 1, single_teams_data.avg_climb_points)
 
                 # Create the chart for cargo scored in auto (high vs. low)
                 cargo_in_auto_chart = output_workbook.add_chart(
@@ -324,3 +447,9 @@ with xlsxwriter.Workbook(output_file_name) as output_workbook:
 
                 single_teams_worksheet.insert_chart(
                     f"{SECOND_CHART_COL}{CHART_START_ROW}", cargo_in_teleop_chart)
+
+                # TODO: Defense Pie Chart
+                # TODO: Hangar Pie Chart
+                # TODO: Hangar level across matches
+                # TODO: Extremely fancy graphs that look absurd
+                # TODO: Sheet for ranks by category
